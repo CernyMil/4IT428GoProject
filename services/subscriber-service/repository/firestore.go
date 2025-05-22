44d30d7a514867e5b37e2398d9753ab085ceef97
Alena-Stef <allieny@seznam.cz>
2025-05-23 01:20:20 +0200

    Env variables, gitignore and firebase initialization adjusments (#11)
    * Update .gitignore to include environment files and remove .env file
    
    * Add FIREBASE_CRED to .env.template for editor service configuration
    
    * Refactor Firebase initialization to use service account credentials directly

--- services/subscriber-service/repository/firestore.go
+++ services/subscriber-service/repository/firestore.go
@@ -3,17 +3,17 @@ package repository
 import (
 	"context"
 	"log"
-	"os"
 
 	"cloud.google.com/go/firestore"
 	firebase "firebase.google.com/go"
+	"google.golang.org/api/option"
 )
 
 func InitializeFirebase() (*firestore.Client, error) {
+	// Use a service account
 	ctx := context.Background()
-	ProjectID := os.Getenv("FIREBASE_PROJECT_ID")
-	conf := &firebase.Config{ProjectID: ProjectID}
-	app, err := firebase.NewApp(ctx, conf)
+	sa := option.WithCredentialsFile("services/subscriber-service/goproject-4c949-firebase-adminsdk-fbsvc-564d6d7e63.json")
+	app, err := firebase.NewApp(ctx, nil, sa)
 	if err != nil {
 		log.Fatalln(err)
 	}
@@ -22,5 +22,23 @@ func InitializeFirebase() (*firestore.Client, error) {
 	if err != nil {
 		log.Fatalln(err)
 	}
+	defer client.Close()
+
 	return client, nil
+	/*
+
+		ctx := context.Background()
+		ProjectID := os.Getenv("FIREBASE_PROJECT_ID")
+		conf := &firebase.Config{ProjectID: ProjectID}
+		app, err := firebase.NewApp(ctx, conf)
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		client, err := app.Firestore(ctx)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		return client, nil
+	*/
 }
