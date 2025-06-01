f3ef5146c2ee4b6dee99f14b7566c57fa425bdf5
Alena Etrichová <allieny@seznam.cz>
2025-06-01 14:40:03 +0200

    Subscriber service should be working correctly now (#43)
    * Subscriber service fixes - should be working correctly
    
    * hot fixes
    
    * hot fix
    
    * hot fix

--- services/subscriber-service/service/newsletter.go
+++ services/subscriber-service/service/newsletter.go
@@ -6,6 +6,14 @@ import (
 	"subscriber-service/pkg/id"
 )
 
+func (s Service) CreateNewsletter(ctx context.Context, newsletterId id.Newsletter) error {
+	err := s.repository.CreateNewsletter(ctx, newsletterId)
+	if err != nil {
+		return err
+	}
+	return err
+}
+
 func (s Service) DeleteNewsletter(ctx context.Context, newsletterId id.Newsletter) error {
 	err := s.repository.DeleteNewsletter(ctx, newsletterId)
 	if err != nil {
