c0436bd8ac5071172501b12fb0a4265ae2046f26
Alena-Stef <allieny@seznam.cz>
2025-06-01 14:36:46 +0200

    hot fix

--- services/newsletter-service/cmd/api/config.go
+++ services/newsletter-service/cmd/api/config.go
@@ -20,9 +20,9 @@ var (
 
 type Config struct {
 	Port         int    `env:"PORT" validate:"required"`
-	AuthSecret   string `env:"AUTH_SECRET"`
-	DatabaseURL  string `env:"DATABASE_URL"`
-	FirebaseCred string `env:"FIREBASE_CRED"`
+	AuthSecret   string `env:"AUTH_SECRET" validate:"required"`
+	DatabaseURL  string `env:"DATABASE_URL" validate:"required,url"`
+	FirebaseCred string `env:"FIREBASE_CRED" validate:"required"`
 }
 
 func init() {
